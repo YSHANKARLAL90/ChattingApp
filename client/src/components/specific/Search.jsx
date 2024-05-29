d4e6f795570fc57b77c6eb5f54b72a1d52ccffb0
Shankar <Shankar@mnnit.ac.in>
2024-05-29 12:02:38 +0530

    Search Page added

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Dialog,DialogTitle,InputAdornment,Stack, TextField,List, ListItem,ListItemText } from '@mui/material'
+import {useInputValidation} from "6pp"
+import React, { useState } from 'react'
+import {Search as SearchIcon} from '@mui/icons-material'
+import UserItem from '../shared/UserItem';
+import { sampleUsers } from '../../constants/sampleData';
+
+// const users = [1,2,3];
 
 export default function Search() {
-  return (
-    <div>
-      Search
-    </div>
-  )
+
+
+  let isLoadingSendFriendRequest = false;
+  const [users,setUsers] = useState(sampleUsers);
+  const addFriendHandler = (id)=>{
+    console.log(id);
+  }
+  const search = useInputValidation("")
+  return <Dialog open>
+    <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+      <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+      <TextField 
+         label="" 
+         value={search.value} 
+         onChange={search.changeHandler}
+         variant="outlined"
+         size="small"
+         InputProps={{
+          startAdornment:(
+            <InputAdornment position="start"><SearchIcon/></InputAdornment>
+          )
+         }}
+         />
+
+    <List>
+      {
+        users.map((i) => (
+          <UserItem 
+            user={i} 
+            key={i._id} 
+            handler={addFriendHandler} 
+            handlerIsLoading={isLoadingSendFriendRequest}
+          />
+        ))
+      }
+    </List>
+    </Stack>
+  </Dialog>
 }
--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -16,3 +16,16 @@ export const sampleChats = [{
     members:["1","2"],
 }
 ]
+
+
+export const sampleUsers = [{
+    avatar : ["https://www.w3schools.com/howto/img_avatar.png"],
+    name:"John Doe",
+    _id:"1",
+},
+{
+    avatar : ["https://www.w3schools.com/howto/img_avatar.png"],
+    name:"John Boe",
+    _id:"2",
+}
+]
